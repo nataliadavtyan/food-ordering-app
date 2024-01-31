37aa44942fdfb31663ca386716ce6a4c862f6ced
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-01-31 20:19:34 +0100

    Make the code dryer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,52 +1,48 @@
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
+const Navigation = styled.nav`
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2em;
+`
+
+const StyledLink = styled(NavLink)`
+    text-decoration: none;
+    color: inherit;
+    font-size: 1rem;
+    font-weight: 400;
+`
+
 export const Nav = () => {
-    const Navigation = styled.nav`
-        list-style: none;
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-        gap: 2em;
-    `
-        
-    // const NavList = styled.ul`
-    // `
-        
-    // const ListItem = styled.li`
-    // `
-    
-    const StyledLink = styled(NavLink)`
-        text-decoration: none;
-        color: inherit;
-        font-size: 1rem;
-        font-weight: 400;
-    `
+    const links = [
+        {
+            link: "Home",
+            to: "/"
+        },
+        {
+            link: "Order",
+            to: "/order"
+        },
+        {
+            link: "Contact",
+            to: "/contact"
+        },
+        {
+            link: "My Profile",
+            to: "/my-profile"
+        },
+    ]
 
     return (
         <Navigation>
-            {/* <NavList> */}
-                {/* <ListItem> */}
-                    <StyledLink to="/">
-                        Main
-                    </StyledLink>
-                {/* </ListItem> */}
-                {/* <ListItem> */}
-                    <StyledLink to="/order">
-                        Order
-                    </StyledLink>    
-                {/* </ListItem> */}
-                {/* <ListItem> */}
-                    <StyledLink to="/contact">
-                        Contact
-                    </StyledLink>
-                {/* </ListItem> */}
-                {/* <ListItem> */}
-                    <StyledLink to="/my-profile">
-                        My Profile
-                    </StyledLink>
-                {/* </ListItem> */}
-            {/* </NavList> */}
+            {links.map((link, i) => (
+                <StyledLink to={link.to} key={`navlink-${i}`}>
+                    {link.link}
+                </StyledLink>
+            ))}
         </Navigation>
     )
 }
