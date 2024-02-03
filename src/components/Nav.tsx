be4aaf2d37c388e408734e68aa6bc16960a78963
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-02-03 15:35:20 +0100

    Add Prettier to format on save

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,48 +1,48 @@
-import styled from 'styled-components'
-import { NavLink } from 'react-router-dom'
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 const Navigation = styled.nav`
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2em;
-`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2em;
+`;
 
 const StyledLink = styled(NavLink)`
-    text-decoration: none;
-    color: inherit;
-    font-size: 1rem;
-    font-weight: 400;
-`
+  text-decoration: none;
+  color: inherit;
+  font-size: 1rem;
+  font-weight: 400;
+`;
 
 export const Nav = () => {
-    const links = [
-        {
-            link: "Home",
-            to: "/"
-        },
-        {
-            link: "Order",
-            to: "/order"
-        },
-        {
-            link: "Contact",
-            to: "/contact"
-        },
-        {
-            link: "My Profile",
-            to: "/my-profile"
-        },
-    ]
+  const links = [
+    {
+      link: "Home",
+      to: "/",
+    },
+    {
+      link: "Order",
+      to: "/order",
+    },
+    {
+      link: "Contact",
+      to: "/contact",
+    },
+    {
+      link: "My Profile",
+      to: "/my-profile",
+    },
+  ];
 
-    return (
-        <Navigation>
-            {links.map((link, i) => (
-                <StyledLink to={link.to} key={`navlink-${i}`}>
-                    {link.link}
-                </StyledLink>
-            ))}
-        </Navigation>
-    )
-}
+  return (
+    <Navigation>
+      {links.map((link, i) => (
+        <StyledLink to={link.to} key={`navlink-${i}`}>
+          {link.link}
+        </StyledLink>
+      ))}
+    </Navigation>
+  );
+};
