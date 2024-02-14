a3e08507b826a0adbea3ce8e6e68e446a9cff21c
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-02-14 18:28:00 +0100

    Create AddToBasketPanel to menu items with minimal styling

--- src/pages/Order/MenuItem.tsx
+++ src/pages/Order/MenuItem.tsx
@@ -1,19 +1,79 @@
 import { FC } from "react";
 import styled from "styled-components";
 import PizzaImgFallback from "../../assets/pizza-img-fallback.jpg";
-import { Pizza } from "./Order";
+import { Pizza } from "./OrderPage";
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
 
 const StyledMenuItem = styled.div`
-  border: 1px solid red;
+  /* border: 1px solid red; */
+  width: 100%;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 16px;
+  background-color: white;
+  justify-self: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.md}) {
+    width: 100%;
+    /* border: 1px solid green; */
+  }
 
   img {
-    width: 100px;
-    height: 100px;
+    border-top-left-radius: 16px;
+    border-top-right-radius: 16px;
+    width: 100%;
+    height: 150px;
     object-fit: cover;
+  }
+
+  div.menu-item-description {
+    padding: 1em 0.75em;
+    /* border: 1px solid red; */
+    position: relative;
+  }
+
+  h3 {
+    margin-bottom: 1.5em;
+  }
+`;
+
+const AddToBasketPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5em;
+  position: absolute;
+  bottom: 1em;
+  right: 0.75em;
+  /* border: 1px solid red; */
+  /* margin: 0;  */
+
+  button {
+    font-size: 1rem;
+    border: 1px solid lightgray;
+    /* border: none; */
+    border-radius: 50%;
+    /* background: lightgray; */
+    background: transparent;
+    padding: 0.5em;
+    /* width: 1.25em;
+    height: 1.25em; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  p {
+    font-size: 1.75rem;
   }
 `;
 
 export const MenuItem: FC<Pizza> = ({ name, price, img }) => {
+  const priceInEuro = (price / 10).toFixed(2);
+
   return (
     <StyledMenuItem>
       <img
@@ -23,8 +83,19 @@ export const MenuItem: FC<Pizza> = ({ name, price, img }) => {
           currentTarget.src = PizzaImgFallback;
         }}
       />
-      <h3>{name}</h3>
-      <p>{price}</p>
+      <div className="menu-item-description">
+        <h3>{name}</h3>
+        <p>€{priceInEuro}</p>
+        <AddToBasketPanel>
+          <button>
+            <FaMinus />
+          </button>
+          <p>0</p>
+          <button>
+            <FaPlus />
+          </button>
+        </AddToBasketPanel>
+      </div>
     </StyledMenuItem>
   );
 };
