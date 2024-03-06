e92da735a2a1cf1c4809e5b3f9418dc40b0f2402
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-03-06 22:20:18 +0100

    Integrate Google maps to the Contact page

---
+++ src/pages/Contact/Map.tsx
@@ -0,0 +1,13 @@
+export const Map = () => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95780.66762224077!2d2.0577883726956476!3d41.39263855811656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49816718e30e5%3A0x44b0fb3d4f47660a!2z0JHQsNGA0YHQtdC70L7QvdCw!5e0!3m2!1sru!2ses!4v1709759168076!5m2!1sru!2ses"
+      width="600"
+      height="450"
+      //   style="border:0;"
+      //   allowfullscreen=""
+      loading="lazy"
+      //   referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+};
