2d39b40290b1cd82b3e0f1db13de9bd9dee47c20
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-03-06 22:19:46 +0100

    Add Contact page basic template

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import { ContactForm } from "./ContactForm";
+import { Map } from "./Map";
+// import { Center } from "../Main/MainContent";
+
+export const ContactPage = styled.div`
+  width: 95%;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h2`
+  font-size: 2rem;
+`;
+
 export const Contact = () => {
-    return (
-        <h2>Contact component is here</h2>
-    )
-}
+  return (
+    <ContactPage>
+      <Title>Contact Us</Title>
+      <p>
+        Please note that this is a fictional restaurant which means that it does
+        not exist. If you are looking for a place to eat a good pizza in
+        Barcelona on Sunday, one of my favourite places is Parking Pizza.
+      </p>
+      <ContactForm />
+      <Map />
+    </ContactPage>
+  );
+};
