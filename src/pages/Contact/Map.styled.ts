7dcb6c965ada6045dc852ce5af87c364fc82c5b3
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-03-18 09:15:33 +0100

    Style the map

---
+++ src/pages/Contact/Map.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledMap = styled.iframe`
+  /* border: 2px solid blue; */
+  border: none;
+  border-radius: 16px;
+  width: 100%;
+  max-width: 500px;
+  min-height: 450px;
+`;
