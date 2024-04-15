51e7714d5a476d2c0ad99322e393eed03276d571
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-15 18:05:26 +0200

    Add react-query to the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
