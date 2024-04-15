e055c9aefd20aed2736e218a551e87cd4b8e9e98
Natalia Davtyan <natalia.davtyan@gmail.com>
2024-04-15 18:04:57 +0200

    Add cupcakes to the list of desserts

--- src/pages/Order/utils/filterFetchedData.ts
+++ src/pages/Order/utils/filterFetchedData.ts
@@ -5,27 +5,23 @@ export const filterFetchedData = (
   data: FetchedMenuItem[],
   category: string
 ) => {
-  // Remove pizzerias and apizzas
   if (category === "pizzas") {
     const pizzasOnly = data.filter((item: FetchedMenuItem) =>
       item.name.toLowerCase().split(" ").includes("pizza")
     );
     return removeDuplicates(pizzasOnly);
   } else if (category === "desserts") {
-    // Only get selected categories of desserts
     const selectedDesserts = data.filter((item: FetchedMenuItem) => {
       const splitItemName = item.name.toLowerCase().split(" ");
       const specificDessertsOnly =
         splitItemName.includes("brownie") ||
         splitItemName.includes("chocolate") ||
         splitItemName.includes("pie") ||
-        splitItemName.includes("cheesecake") ||
-        splitItemName.includes("cupcake");
+        splitItemName.includes("cheesecake");
       return specificDessertsOnly;
     });
     return removeDuplicates(selectedDesserts);
   } else if (category === "drinks") {
-    // Only get selected categories of drinks
     const selectedDrinks = data.filter((item: FetchedMenuItem) => {
       const splitItemName = item.name.toLowerCase().split(" ");
       const specificDrinksOnly =
